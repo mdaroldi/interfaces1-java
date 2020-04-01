bb50a3c90034b3f3dadc3c0308440566a6131315
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2020-04-01 11:39:37 -0300

    Create static variables for PaypalService

--- src/services/PaypalService.java
+++ src/services/PaypalService.java
@@ -3,14 +3,17 @@ package services;
 
 public class PaypalService implements OnlinePaymentService {
 	
+	private static final double FEE = 0.02;
+	private static final double INTEREST_RATE = 0.01;
+	
 	public PaypalService() {
 	}
 	
 	public double paymentFee(double amount) {
-		return amount * 0.02;
+		return amount * FEE;
 	}
 	
 	public double interest(double amount, int months) {
-		return (amount * 0.01 * months);
+		return (amount * INTEREST_RATE * months);
 	}
 }
