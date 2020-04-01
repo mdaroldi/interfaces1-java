85bab6e774f44a560201600921765c4802a38da8
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2020-04-01 11:31:28 -0300

    Implement methods and constructors

--- src/entities/Contract.java
+++ src/entities/Contract.java
@@ -1,5 +1,52 @@
 package entities;
 
-public class Contract {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Contract {
+	private int number;
+	private Date date;
+	private double totalValue;
+	
+	private List<Installment> installment = new ArrayList<>();
+	
+	public Contract() {
+	}
+
+	public Contract(int number, Date date, double totalValue) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(double totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public void addInstallment(Installment installment) {
+		this.installment.add(installment);
+	}
+	
+	
 }
--- src/services/PaypalService.java
+++ src/services/PaypalService.java
@@ -1,15 +1,16 @@
 package services;
 
+
 public class PaypalService implements OnlinePaymentService {
 	
 	public PaypalService() {
 	}
 	
 	public double paymentFee(double amount) {
-		return 0;
+		return amount * 0.02;
 	}
 	
 	public double interest(double amount, int months) {
-		return 0;
+		return (amount * 0.01 * months);
 	}
 }
