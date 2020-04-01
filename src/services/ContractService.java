02c0f139134e3b672e82df8b639a4d682a2c797f
Mauricio Aroldi <mauricio.aroldi@gmail.com>
2020-04-01 11:34:22 -0300

    Update method processContract

--- src/services/ContractService.java
+++ src/services/ContractService.java
@@ -1,12 +1,32 @@
 package services;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import entities.Contract;
+import entities.Installment;
 
 public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+	
 	public ContractService() {
 	}
 	
 	public void processContract(Contract contract, int months) {
-		
+		double result = contract.getTotalValue() / months;
+		for(int i = 1; i <= months; i++) {
+			Date date = addMonths(contract.getDate(), i);
+			double updatedResult = result + onlinePaymentService.interest(result, i);
+			double total = updatedResult + onlinePaymentService.paymentFee(updatedResult);
+			contract.addInstallment(new Installment(date, total));
+		}
+	}
+	
+	private Date addMonths(Date date, int n) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(Calendar.MONTH, n);
+		return cal.getTime();
 	}
 }
